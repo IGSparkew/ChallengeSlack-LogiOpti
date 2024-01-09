f60e25853e1ccb106c0093b62f8126ccc3def475
Mohamed <mohamed.djabi@outlook.fr>
2024-01-09 16:57:22 +0100

    Layout

--- front-end/logi-opti/src/app/client/page.js
+++ front-end/logi-opti/src/app/client/page.js
@@ -1,8 +1,114 @@
-
+import Image from 'next/image'
 export default function Client() {
-    return(
+    return (
         <main>
-            <h1>Client View</h1>
+            
+            <div className="min-h-screen bg-gray-50/50">
+                <aside className="bg-gray-300 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
+                    <div className="relative border-b border-white/20">
+                        <a className="flex items-center gap-4 py-6 px-8" href="#/">     </a>
+                        
+           
+                 
+                        <Image
+      src="/logo.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+                     
+                        <button className="middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden" type="button">
+                            <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" aria-hidden="true" className="h-5 w-5 text-white">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                    <div className="m-4">
+                        <ul className="mb-4 flex flex-col gap-1">
+                            {/* BOUTON DASHBOARD */}
+                        <li>
+          <a aria-current="page" class="active" href="#">
+            <button class="middle none font-sans font-bold center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-black shadow-md shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] w-full flex items-center gap-4 px-4 capitalize" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-5 h-5 text-inherit">
+                <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"></path>
+                <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"></path>
+              </svg>
+              <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">dashboard</p>
+            </button>
+          </a>
+        </li>
+           {/* BOUTON STATISTIQUE */}
+
+           <li>
+          <a class="" href="#">
+            <button class="middle none font-sans font-bold center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-black hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-5 h-5 text-inherit">
+              <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"></path>
+              </svg>
+              <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">Statistique</p>
+            </button>
+          </a>
+        </li>
+                                   {/* BOUTON PROFILE */}
+        <li>
+          <a class="" href="#">
+            <button class="middle none font-sans font-bold center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-black hover:bg-white/10 active:bg-white/30 w-full flex items-center gap-4 px-4 capitalize" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="w-5 h-5 text-inherit">
+                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd"></path>
+              </svg>
+              <p class="block antialiased font-sans text-base leading-relaxed text-inherit font-medium capitalize">profile</p>
+            </button>
+          </a>
+        </li>
+     
+
+                        </ul>
+                        <ul className="mb-4 flex flex-col gap-1">
+                            {/* ... Plusieurs éléments <li> similaires ... */}
+                        </ul>
+                    </div>
+                </aside>
+                <div className="p-4 xl:ml-80">
+                    <nav class="block w-full max-w-full bg-transparent text-white shadow-none rounded-xl transition-all px-0 py-1">
+	
+	
+      <div class="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
+        <div class="capitalize">
+          <nav aria-label="breadcrumb" class="w-max">
+            <ol class="flex flex-wrap items-center w-full bg-opacity-60 rounded-md bg-transparent p-0 transition-all">
+              <li class="flex items-center text-blue-gray-900 antialiased font-sans text-sm font-normal leading-normal cursor-pointer transition-colors duration-300 hover:text-light-blue-500">
+                <a href="#">
+                  <p class="block antialiased font-sans text-sm leading-normal text-blue-900 font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100">dashboard</p>
+                </a>
+                <span class="text-gray-500 text-sm antialiased font-sans font-normal leading-normal mx-2 pointer-events-none select-none">/</span>
+              </li>
+              <li class="flex items-center text-blue-900 antialiased font-sans text-sm font-normal leading-normal cursor-pointer transition-colors duration-300 hover:text-blue-500">
+                <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">home</p>
+              </li>
+            </ol>
+          </nav>
+          </div>
+          </div>
+          </nav>
+                    <div className="mt-12">
+                        <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+                            {/* ... Plusieurs éléments <div> similaires ... */}
+                        </div>
+                        <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
+                            <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
+                                {/* ... Contenu de la table ... */}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-blue-gray-600">
+                        <footer className="py-2">
+                            {/* ... Contenu du pied de page ... */}
+                        </footer>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
