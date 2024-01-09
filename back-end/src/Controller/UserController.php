1cbe46bf7d0e8e05bab9d00c15c1b60d6680f672
IGSparkew <alex.robbrecht@sfr.fr>
2024-01-09 17:06:34 +0100

    get user

--- back-end/src/Controller/UserController.php
+++ back-end/src/Controller/UserController.php
@@ -4,18 +4,32 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class UserController extends AbstractController
 {
+    public function __construct(private TokenStorageInterface $tokenStorage) { }
+
     #[Route('/api/user/role', name: 'app_role', methods: ["GET"])]
     public function getRole(#[CurrentUser] ?User $user): Response
     {
-        if($user == null){
+        //if($user == null){
+        //    return $this->json(["message"=>"User not found"], Response::HTTP_UNAUTHORIZED);
+        //}
+        $token = $this->tokenStorage->getToken();
+        if(empty($token)) {
+            return $this->json(["message"=>"Token not found"], Response::HTTP_UNAUTHORIZED);
+        }
+        
+        $user = $token->getUser();
+
+        if (empty($user)) {
             return $this->json(["message"=>"User not found"], Response::HTTP_UNAUTHORIZED);
         }
         return $this->json(["role"=>$user->getRoles()]);
+
     }
 }
