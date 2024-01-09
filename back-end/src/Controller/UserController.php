ece0f7e4ce7a34c66e8a153d163a2f5b6b357f43
Diaby <bac.diaby@gmail.com>
2024-01-09 16:45:09 +0100

    UserController

---
+++ back-end/src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/user/role', name: 'app_role', methods: ["GET"])]
+    public function getRole(#[CurrentUser] ?User $user): Response
+    {
+        if($user == null){
+            return $this->json(["message"=>"User not found"], Response::HTTP_UNAUTHORIZED);
+        }
+        return $this->json(["role"=>$user->getRoles()]);
+    }
+}
