10c9aec7ebe44d5e6c8584d97417973a4418e8fc
IGSparkew <alex.robbrecht@sfr.fr>
2024-01-09 11:59:42 +0100

    add logo

--- front-end/logi-opti/src/app/components/login.js
+++ front-end/logi-opti/src/app/components/login.js
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image'
 
 export default function Login() {
 
@@ -25,7 +26,13 @@ export default function Login() {
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
             <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-                <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>
+            <div className="flex flex-wrap justify-center">
+            <img
+                className="w-1/2 my-6"
+                src="/logo.png"
+                alt="logo logiOpti"
+                />
+            </div>
                 <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
                     <form onSubmit={authSubmit} className="px-5 py-7">
                         <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
