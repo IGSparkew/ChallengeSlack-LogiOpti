10eac204a17a32d6a097d14bc08dee21843652dc
Bachekou Diaby <81798735+Bachekou-DIABY@users.noreply.github.com>
2024-01-10 13:40:17 +0100

    Registration feature (#5)

---
+++ back-end/src/Controller/RegisterController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher){
+
+    }
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $data = json_decode($request->getContent(), true);
+        if(!empty($data) &&
+            $data["email"]!=null &&
+            $data["lastName"]!=null &&
+            $data["firstName"]!=null &&
+            $data["password"]!=null &&
+            $data["salary"]!=null &&
+            !empty($data["roles"])
+        )
+        {
+            $user->setEmail($data["email"]);
+            $plainPassword = $data["password"];
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $plainPassword
+            );
+            $user->setPassword($hashedPassword);
+            $user->setLastName($data["lastName"]);
+            $user->setFirstName($data["firstName"]);
+            $user->setRoles($data["roles"]);
+            $user->setSalary($data["salary"]);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->json([
+                'message'=>'L\'utilisateur a bien été enregistré dans la base de données'
+            ],Response::HTTP_CREATED);
+        }
+
+        return $this->json([
+            'message'=>'Le formulaire d\'inscription est incomplet ou contient des données invalides'
+        ],Response::HTTP_BAD_REQUEST);
+    }
+}
