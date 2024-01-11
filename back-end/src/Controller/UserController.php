31ea432c2fdae2a5ebba706dcc687e29a16de34a
Diaby <bac.diaby@gmail.com>
2024-01-11 17:34:01 +0100

    get user

--- back-end/src/Controller/UserController.php
+++ back-end/src/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Middleware\AuthentificationMiddleware;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,6 +24,30 @@ class UserController extends AbstractController
         }
 
         return $this->json(["role"=>$this->authentificationMiddleware->getRole($request)]);
+    }
+
+    #[Route('/api/user/get/{id}', name: 'app_get', methods: ["GET"])]
+    public function get(Request $request, EntityManagerInterface $entityManager, int $id): jsonResponse
+    {
+
+        if (!$this->authentificationMiddleware->verify($request)) {
+            return $this->json([
+                'message' => 'You are not authentified or doesn\'t have the right to access this page'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $data = json_decode($request->getContent(), true);
+
+        if (!empty($id)) {
+            $user = $entityManager->getRepository(User::class)->find($id);
+            if ($user) {
+                $userObject = new User();
+                return $this->json([$userObject->convertUserEntityToArray($user)]);
+            }
+            throw $this->createNotFoundException(
+                'User not found' . $user
+            );
+        }
+        return $this->json(['message' => "Missing Id"], 500);
     }
 }
 
