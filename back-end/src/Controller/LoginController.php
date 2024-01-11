1dc92f4108f675ea6276ec14f3589c21dada4bc9
IGSparkew <alex.robbrecht@sfr.fr>
2024-01-11 12:15:53 +0100

    fix login add middleware

--- back-end/src/Controller/LoginController.php
+++ back-end/src/Controller/LoginController.php
@@ -3,27 +3,49 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Repository\UserRepository;
+use App\Services\AuthenticationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+
+
 
 class LoginController extends AbstractController
 {
+
+    public function __construct(private UserRepository $userRepository, private UserPasswordHasherInterface $userPasswordHasherInterface, private JWTTokenManagerInterface $JWTManager) { }
+
     #[Route('/login', name: 'login', methods: ["POST"])]
-    public function login(#[CurrentUser] ?User $user): Response
+    public function login(Request $request): Response
     {
-        if ($user == null) {
-            return $this->json([
-                'message'=>'User not found'
-            ],Response::HTTP_UNAUTHORIZED);
+        $data = json_decode($request->getContent(), true);
+
+        if ($data == null) {
+            return $this->json("body not found",Response::HTTP_UNAUTHORIZED);
         }
 
-        $token = "";
-        return $this->json([
-            'user' => $user->getUserIdentifier(),
-            'token' => $token,
-        ]);
+        $email = $data["username"];
+        $password = $data["password"];
+
+        if (empty($email) || empty($password)) {
+            return $this->json("body not found",Response::HTTP_UNAUTHORIZED);
+        }
+
+        $userFound = $this->userRepository->findOneBy(["email" => $email]);
+
+        if ($userFound == null) {
+            return $this->json("user not found",Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($this->userPasswordHasherInterface->isPasswordValid($userFound, trim($password))) {
+            return $this->json($this->JWTManager->create($userFound));
+        }
+
+        return $this->json("password not correct",Response::HTTP_UNAUTHORIZED);
     }
 
 }
--- back-end/src/Controller/UserController.php
+++ back-end/src/Controller/UserController.php
@@ -3,22 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Middleware\AuthentificationMiddleware;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class UserController extends AbstractController
 {
-    public function __construct() { }
+    public function __construct(private AuthentificationMiddleware $authentificationMiddleware, private UserRepository $userRepository) { }
 
     #[Route('/api/user/role', name: 'app_role', methods: ["GET"])]
-    public function getRole(#[CurrentUser] ?User $user): Response
+    public function getRole(Request $request): Response
     {
-        if($user == null){
-            return $this->json(["message"=>"User not found"], Response::HTTP_UNAUTHORIZED);
+        if (!$this->authentificationMiddleware->verify($request)) {
+            return $this->json(["error"=>"Your are not connected or doesn't have token"],Response::HTTP_UNAUTHORIZED);
         }
 
-        return $this->json(["role"=>$user->getRoles()]);
+        return $this->json(["role"=>$this->authentificationMiddleware->getRole($request)]);
     }
 }
