14831289050818f774fcb60f6f5742da54ba7fbd
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2024-01-11 12:43:53 +0100

    Backend clean (#10)

---
+++ back-end/README.md
@@ -0,0 +1,7 @@
+# Symfony API
+
+## How to use 
+
+- use ```php bin/console lexik:jwt:generate-keypair``` to genrate jwt token in API
+
+- add JWT_PASSPHRASE in .env 
---
+++ back-end/src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class UserController extends AbstractController
+{
+    public function __construct(private TokenStorageInterface $tokenStorage) { }
+
+    #[Route('/api/user/role', name: 'app_role', methods: ["GET"])]
+    public function getRole(#[CurrentUser] ?User $user): Response
+    {
+        if($user == null){
+            return $this->json(["message"=>"User not found"], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json(["role"=>$user->getRoles()]);
+    }
+}
+
+//CRUD complet
