cd487ce332314ad979ea1f7906435d9a5d5316a2
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2024-01-11 18:02:10 +0100

    ajout fixtures

--- back-end/config/bundles.php
+++ back-end/config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
