2989b41b052675712d54124a7f1f8b76d35a798c
Diaby <bac.diaby@gmail.com>
2024-01-12 09:15:39 +0100

    page trajet

--- front-end/logi-opti/src/app/driver/page.js
+++ front-end/logi-opti/src/app/driver/page.js
@@ -10,16 +10,17 @@ import Titre from "./components/Titre";
 import { useState, useEffect } from "react";
 import { isDriverUser } from "../middleware/authMiddleware";
 import { useRouter } from "next/navigation";
+import {ApiService} from "@/app/services/apiService";
 
 export default function Driver() {
 
     const [openDetails,setOpenDetails] = useState(false);
+    const [details,setDetails] = useState([]);
     const [openArrivee,setOpenArrivee] = useState(false);
     const [openAjout,setOpenAjout] = useState(false);
     const [openUpdate,setOpenUpdate] = useState(false);
-
-
     const router = useRouter();
+    const api = new ApiService();
 
     const handleSetOpenDetails = (data) => {
         setOpenDetails(data);
@@ -34,12 +35,56 @@ export default function Driver() {
         setOpenUpdate(data);
     }
 
-
     useEffect(() => {
         if (!isDriverUser()) {
             router.push('/', "push");
         }
+        const token = localStorage.getItem("token");
+        api.get('/api/delivery/get', token)
+            .then((data) => {
+                const trajetData = getTrajet(data);
+                setDetails(trajetData);
+            })
     }, []);
+
+    function getTrajet(data) {
+        const trajet = [];
+
+        data.forEach((element) => {
+            var startDate = element.start_date.date.split('-')
+            const startYear = startDate[0]
+            const startMonth = startDate[1]
+            startDate = startDate[2].split(' ')
+            const startDay = startDate[0]
+            startDate = startDate[1].split(":")
+            const startHour = startDate[0]
+            const startMin = startDate[1]
+            var endDate = element.end_date.date.split('-')
+            const endYear = endDate[0]
+            const endMonth = endDate[1]
+            endDate = endDate[2].split(' ')
+            const endDay = endDate[0]
+            endDate = endDate[1].split(":")
+            const endHour = endDate[0]
+            const endMin = endDate[1]
+            trajet.push({
+                lastname: element.user.lastname,
+                firstname: element.user.firstname,
+                startStreet: element.startAddress.street,
+                startPostalCode: element.startAddress.postalCode,
+                endStreet: element.startAddress.street,
+                endPostalCode: element.startAddress.postalCode,
+                startDate: `${startYear}/${startMonth}/${startDay} ${startHour}H${startMin}`,
+                endDate: `${endYear}/${endMonth}/${endDay} ${endHour}H${endMin}`,
+                vehicleType: element.vehicle.vehicleType.type,
+                tollCost : element.toll_cost,
+                usingCost : element.using_cost,
+                distance : element.distance,
+                time : element.Time,
+            });
+        });
+        return trajet;
+    }
 
     return(
         <main>
@@ -51,7 +96,9 @@ export default function Driver() {
             </div>
 
             <ModalArrivee openArrivee={openArrivee} setOpenArrivee={setOpenArrivee}/>
-            <ModalDetails openDetails={openDetails} setOpenDetails={setOpenDetails}/>
+            {details.map((item, index) => (
+                <ModalDetails key={index} openDetails={openDetails} setOpenDetails={setOpenDetails} details={item} setDetails={setDetails}/>
+            ))}
             <ModalAjoutUpdate openAjout={openAjout} openUpdate={openUpdate} setOpenAjout={setOpenAjout} setOpenUpdate={setOpenUpdate}/>
         </main>
     );
